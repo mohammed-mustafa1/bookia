970309dfe87f29b29ae4389ca3380e9244f42eec
muhammad-mustafa1 <mohammedmustafa17798@gmail.com>
2025-06-30 07:47:08 +0300

    create shimmer ui

--- lib/features/home/presentation/pages/home_screen.dart
+++ lib/features/home/presentation/pages/home_screen.dart
@@ -6,10 +6,13 @@ import 'package:bookia/features/home/presentation/cubit/home_cubit.dart';
 import 'package:bookia/features/home/presentation/cubit/home_state.dart';
 import 'package:bookia/features/home/presentation/widgets/best_seller_grid_view.dart';
 import 'package:bookia/features/home/presentation/widgets/home_slider.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:gap/gap.dart';
+import 'package:shimmer/shimmer.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -43,14 +46,15 @@ class HomeScreen extends StatelessWidget {
             Gap(12)
           ],
         ),
-        body: SingleChildScrollView(
-          child: BlocBuilder<HomeCubit, HomeState>(
-            builder: (context, state) {
-              var cubit = context.read<HomeCubit>();
-              if (state is HomeError) {
-                return Center(child: Text(state.message));
-              } else if (state is HomeSuccess) {
-                return Column(
+        body: BlocBuilder<HomeCubit, HomeState>(
+          builder: (context, state) {
+            var cubit = context.read<HomeCubit>();
+            if (state is HomeError) {
+              return Text(state.message);
+            } else if (state is HomeSuccess) {
+              return SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Padding(
@@ -68,12 +72,98 @@ class HomeScreen extends StatelessWidget {
                       ),
                     )
                   ],
-                );
-              } else {
-                return Center(child: CircularProgressIndicator());
-              }
-            },
-          ),
+                ),
+              );
+            } else {
+              return emptyUi();
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  Shimmer emptyUi() {
+    return Shimmer.fromColors(
+      baseColor: Colors.grey.shade300,
+      highlightColor: Colors.grey.shade100,
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Gap(20),
+                  CarouselSlider.builder(
+                    itemCount: 4,
+                    itemBuilder: (_, index, realIndex) {
+                      return Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: AppColors.primaryColor,
+                        ),
+                      );
+                    },
+                    options: CarouselOptions(
+                        aspectRatio: 4 / 1.8,
+                        viewportFraction: 1,
+                        initialPage: 0,
+                        enableInfiniteScroll: true,
+                        reverse: false,
+                        autoPlay: false,
+                        autoPlayInterval: Duration(seconds: 3),
+                        autoPlayAnimationDuration: Duration(milliseconds: 800),
+                        autoPlayCurve: Curves.fastOutSlowIn,
+                        enlargeCenterPage: true,
+                        enlargeFactor: 0.25,
+                        scrollDirection: Axis.horizontal,
+                        scrollPhysics: NeverScrollableScrollPhysics()),
+                  ),
+                  Gap(14),
+                  SmoothPageIndicator(
+                    controller:
+                        PageController(initialPage: 0), // PageController
+                    count: 4,
+                    effect: ExpandingDotsEffect(
+                      dotHeight: 7,
+                      dotWidth: 7,
+                      spacing: 4,
+                      expansionFactor: 6,
+                      activeDotColor: AppColors.primaryColor,
+                      dotColor: Color(0xffD9D9D9),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Gap(32),
+            Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Text('Best Sellers', style: TextStyles.getHeadLine2())),
+            Padding(
+                padding: const EdgeInsets.all(16),
+                child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 0.58,
+                    mainAxisSpacing: 12,
+                    crossAxisSpacing: 12,
+                  ),
+                  itemBuilder: (context, index) {
+                    return Container(
+                      decoration: BoxDecoration(
+                        color: AppColors.whiteColor,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    );
+                  },
+                  itemCount: 6,
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                ))
+          ],
         ),
       ),
     );
